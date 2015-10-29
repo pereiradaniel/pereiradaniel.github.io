09ebea36ef1133e5eedf0755b0d6a08275ddcfa6
Daniel Pereira <pereiradpg@yahoo.ca>
2015-10-29 17:11:02 -0400

    Add jQuery for buttons to function.
    Item buttons display/hide pages.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,21 @@
-
-
 $(document).ready(function() {
 
+	var lastItemClicked = undefined;
+
 	$("#item1").click(function() {
-		$(".content").toggleClass("notvisible");
+		clearPageAndDisplayClickedItem($(".item1content"));
 	});
 
+	$("#item2").click(function() {
+		clearPageAndDisplayClickedItem($(".item2content"));
+	});
+
+	var clearPageAndDisplayClickedItem = function (itemToDisplay) {
+		if (lastItemClicked != undefined) {
+			lastItemClicked.toggleClass("notvisible");
+		}else{};
+			itemToDisplay.toggleClass("notvisible");
+			lastItemClicked = itemToDisplay;
+	};
 
 });
