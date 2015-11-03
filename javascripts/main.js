051e949cfab4b90486d653a8789c38ef0ae218b8
Daniel Pereira <pereiradpg@yahoo.ca>
2015-11-03 17:05:58 -0500

    Menu buttons and CSS transitions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
 
 	var lastItemClicked = undefined;
+	var lastMenuItemClicked = undefined;
 
 	$("#item1").click(function() {
-		clearPageAndDisplayClickedItem($(".item1content"));
+		clearPageAndDisplayClickedItem($(".item1content"), $(this));
 	});
 
 	$("#item2").click(function() {
-		clearPageAndDisplayClickedItem($(".item2content"));
+		clearPageAndDisplayClickedItem($(".item2content"), $(this));
 	});
 
 	$("#item3").click(function() {
-		clearPageAndDisplayClickedItem($(".item3content"));
+		clearPageAndDisplayClickedItem($(".item3content"), $(this));
 	});
 
 	$("#item4").click(function() {
-		clearPageAndDisplayClickedItem($(".item4content"));
+		clearPageAndDisplayClickedItem($(".item4content"), $(this));
 	});
 
-	var clearPageAndDisplayClickedItem = function (itemToDisplay) {
+	var clearPageAndDisplayClickedItem = function (itemToDisplay, menuItemToBorder) {
 		if (lastItemClicked != undefined) {
 			lastItemClicked.toggleClass("notvisible");
+			lastMenuItemClicked.toggleClass("bordered-item");
 		}else{};
 			itemToDisplay.toggleClass("notvisible");
+			menuItemToBorder.toggleClass("bordered-item");
 			lastItemClicked = itemToDisplay;
+			lastMenuItemClicked = menuItemToBorder;
 	};
 
 });
