bae292c35dacb468c1130f85519454cecb163f0b
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-21 17:30:31 -0400

    Deployments added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,29 +1,2 @@
 $(document).ready(function() {
-
-	var lastItemClicked = undefined;
-	var lastMenuItemClicked = undefined;
-
-	$("#item1").click(function() {
-		clearPageAndDisplayClickedItem($(".item1content"), $(this));
-	});
-
-	$("#item2").click(function() {
-		clearPageAndDisplayClickedItem($(".item2content"), $(this));
-	});
-
-	$("#item3").click(function() {
-		clearPageAndDisplayClickedItem($(".item3content"), $(this));
-	});
-
-	var clearPageAndDisplayClickedItem = function (itemToDisplay, menuItemToBorder) {
-		if (lastItemClicked != undefined) {
-			lastItemClicked.toggleClass("notvisible");
-			lastMenuItemClicked.toggleClass("underlined");
-		}else{};
-			itemToDisplay.toggleClass("notvisible");
-			menuItemToBorder.toggleClass("underlined");
-			lastItemClicked = itemToDisplay;
-			lastMenuItemClicked = menuItemToBorder;
-	};
-
 });
