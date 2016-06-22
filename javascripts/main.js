90e2aa393bcfb42aff2128ae8ff9c255dd7ac83b
Daniel <truenorthtrainingcompany@gmail.com>
2016-06-22 15:19:01 -0400

    Menu close button switches colour and replaces bars

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 		$("#menu").click(function() {
 			$("#menu-mask").toggleClass("masked");
 			$("#menu-options").toggleClass("notvisible");
+			$("#menu").toggleClass("fa-bars");
+			$("#menu").toggleClass("fa-close");
+			$("#menu").toggleClass("menu-close-colour");
 		})
 });
