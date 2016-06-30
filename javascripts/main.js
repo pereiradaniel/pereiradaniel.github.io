aa5adb7622074885e4bf24b314e1f9b35762ad80
Daniel Pereira <pereiradpg@yahoo.ca>
2016-06-30 17:54:13 -0400

    Updates to styles and content

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
 			$("#menu-options").toggleClass("notvisible");
 			$("#menu").toggleClass("fa-bars");
 			$("#menu").toggleClass("fa-close");
-			$("#menu").toggleClass("menu-close-colour");
 		})
 		$("#icon1").click(function() {
 			$("#icon1").toggleClass("fa-spin");
