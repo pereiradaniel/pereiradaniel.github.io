5a7d8ac9bcef168bb13f6842e4c67eb37ddd20e3
Daniel <truenorthtrainingcompany@gmail.com>
2016-09-15 18:10:17 -0400

    Buttons and styles updated Mouse over on bar menu now goes to hand cursor

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-		$("#menu").click(function() {
+		$("#menu").click(function(e) {
+			e.preventDefault();
 			$("#menu-mask").toggleClass("masked");
 			$("#menu-options").toggleClass("notvisible");
 			$("#menu").toggleClass("fa-bars");
 			$("#menu").toggleClass("fa-close");
+      $("#menu").toggleClass("white-80");
 		})
 		$("#icon1").click(function() {
 			$("#icon1").toggleClass("fa-spin");
