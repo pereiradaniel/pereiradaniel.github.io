27643d7205dcbaf9a96c43e6ed7ef3f400fda39e
Daniel Pereira <pereiradpg@yahoo.ca>
2016-10-10 15:46:04 -0400

    Comments added

--- javascripts/about.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$("#icon1").click(function() {
-		$("#icon1").toggleClass("fa-spin");
-	})
-});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
+		// Handles click of mobile hidden menu
 		$("#menu").click(function(e) {
+			// Prevent default action of element
 			e.preventDefault();
+			// Toggles 'masked' class from hidden menu
+			// 		'masked' class masks the browser window with a transparent div element to display #menu-options to the client
 			$("#menu-mask").toggleClass("masked");
+			// Toggles 'notvisible' class
+			//		'notvisible' class sets display property of menu-options to notvisible when menu is not in use
 			$("#menu-options").toggleClass("notvisible");
+			// Toggles menu icon
+			//		Only one of these classes should be active at a time, in order to display either a cross or the three menu bars
 			$("#menu").toggleClass("fa-bars");
 			$("#menu").toggleClass("fa-close");
+			// Toggles colours
+			//		Colours for menu bars and menu close icon are different due to darker menu colour scheme
       $("#menu").toggleClass("white-80");
       $("nav").toggleClass("black-bgnd");
-		})
-		$("#icon1").click(function() {
-			$("#icon1").toggleClass("fa-spin");
 		})
 });
